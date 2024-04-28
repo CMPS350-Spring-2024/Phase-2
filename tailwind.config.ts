0e99d7f68e875bc3173738aa2967f0512869814a
Muhammad Fauzan Aristya Putra <muhammadfauzanaristyaputra@gmail.com>
2024-04-28 17:27:12 +0300

    chore(setup): import configs from phase 1

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,43 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import plugin from 'tailwindcss/plugin';
 
-const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
-};
-export default config;
+/** @type {import('tailwindcss').Config} */
+export default {
+	important: true,
+	content: ['./src/**/*.{html,js,ts,jsx,tsx,mdx}'],
+	theme: {
+		extend: {
+			colors: {
+				title: '#1F2937',
+				secondary: '#6B7280',
+				border: '#E5E7EB',
+				rating: '#EAB308',
+			},
+			fontSize: {
+				'2xs': '.625rem',
+				'xs': '.75rem',
+			},
+			fontFamily: {
+				sans: ['"Source Sans Pro"', 'sans-serif'],
+				serif: ['"Source Serif Pro"', 'serif'],
+				display: ['"Montserrat"', 'sans-serif'],
+			},
+			strokeWidth: {
+				3: '3px',
+				4: '4px',
+				5: '5px',
+				6: '6px',
+			},
+			boxShadow: {
+				sm: '0px 6px 12px 0px rgba(0, 0, 0, 0.03)',
+			},
+		},
+	},
+	plugins: [
+		require('@tailwindcss/forms'),
+		plugin(({ addVariant }) => {
+			addVariant('focus-hover', ['&:focus', '&:hover']);
+			addVariant('group-focus-hover', [':merge(.group):focus &', ':merge(.group):hover &']);
+		}),
+	],
+} as Config;
