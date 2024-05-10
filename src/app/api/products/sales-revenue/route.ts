bc36791ec7bef9a81c228f1999a1df0f03d902c4
marwan-mk2108477 <mk2108477@qu.edu.qa>
2024-05-10 20:18:53 +0300

    fix: change route function name to GET

--- src/app/api/products/sales-revenue/route.ts
+++ src/app/api/products/sales-revenue/route.ts
@@ -1,7 +1,7 @@
 import ProductService from '@/db/ProductService';
 
 // return the total sales revenue within a specific date range.
-export const GET_SALES_REVENUE = async (req: Request) => {
+export const GET = async (req: Request) => {
 	const { startDate, endDate } = req.query;
 	const totalRevenue = await ProductService.calculateTotalSalesRevenueByDateRange(new Date(startDate), new Date(endDate));
 	return Response.json({ totalRevenue });
