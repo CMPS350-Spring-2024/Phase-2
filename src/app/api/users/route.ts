51020eb54c0c6afe717f8514a85f39a0100d37e9
marwan-mk2108477 <mk2108477@qu.edu.qa>
2024-05-10 14:16:04 +0300

    feat(api): add comments

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,5 +1,6 @@
 import ProductService from '@/db/ProductService';
 
+// return the total number of users in the database.
 export const GET = async () => {
 	const totalUsers = await ProductService.calculateTotalUsers();
 	return Response.json({ totalUsers });
