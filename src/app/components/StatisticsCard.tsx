d4960d5319168b54adf4c2b2c7d55dd4f312924e
Mohammed- ma1704262 <ma1704262@qu.edu.qa>
2024-05-11 21:55:11 +0300

    tried to fetch , but didn't work , kept on causing errors

--- src/app/components/StatisticsCard.tsx
+++ src/app/components/StatisticsCard.tsx
@@ -1,4 +1,9 @@
+
+// import TotalOrders from "@/api/products/total-orders/";
 export default function StatisticsCard() {
+	// event.preventDefault();
+
+
 	return (
 		<>
 			<section className='statistics'>
@@ -43,7 +48,8 @@ export default function StatisticsCard() {
 								className='this-stat'
 								id='sale-statistic'
 							>
-								00{/* 41,321 */}
+								{/* {response.json()} */}
+								{/* 41,321 */}
 							</h2>
 
 							<h2 className='currncy text-gray-600'>Drones</h2>
