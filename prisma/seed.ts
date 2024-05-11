b4b880f8d11d46851c9506cf36bc3da4d505668b
Osamah Alsumaitti <152556993+oa2006040@users.noreply.github.com>
2024-05-11 21:34:12 +0300

    add: seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,14 +1,28 @@
-//	@ts-nocheck
+// @ts-nocheck
+import { Prisma, PrismaClient } from '@prisma/client';
 import ProductService from '../src/db/ProductService';
 import UsersService from '../src/db/UserService';
+import { prisma } from '../src/server/db';
 
-import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
+
+async function createRandomCustomers(numCustomers: number) {}
+
+async function createRandomOrders(customers: Prisma.CustomerCreateManyInput[]) {}
+
+async function createRandomTransactions(customers: Prisma.CustomerCreateManyInput[]) {}
 
 async function main() {
 	try {
 		UsersService.addDefaultData();
 		ProductService.addDefaultData();
+
+		const numCustomers = 10;
+		const customers = await createRandomCustomers(numCustomers);
+		const orders = await createRandomOrders(customers);
+		const transactions = await createRandomTransactions(customers);
+
+		console.log(`Created ${customers.length} customers, ${orders.length} orders, and ${transactions.length} transactions.`);
 	} catch (error) {
 		console.log(error);
 		return { error: error.message };
