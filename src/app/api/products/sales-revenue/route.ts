b03ca90bf16b167565c26ccbc81ced8b85a2987b
Muhammad Fauzan Aristya Putra <muhammadfauzanaristyaputra@gmail.com>
2024-05-11 22:22:21 +0300

    fix(build): crashing due to wrong api routes

--- src/app/api/products/sales-revenue/route.ts
+++ src/app/api/products/sales-revenue/route.ts
@@ -2,8 +2,10 @@ import ProductService from '@/db/ProductService';
 
 // return the total sales revenue within a specific date range.
 export const GET = async (req: Request) => {
-	const { startDate, endDate } = req.query;
+	const { searchParams } = new URL(req.url);
+	const startDate = Number(searchParams.get('startDate')) || 0;
+	const endDate = Number(searchParams.get('endDate')) || 0;
+
 	const totalRevenue = await ProductService.calculateTotalSalesRevenueByDateRange(new Date(startDate), new Date(endDate));
 	return Response.json({ totalRevenue });
 };
-
